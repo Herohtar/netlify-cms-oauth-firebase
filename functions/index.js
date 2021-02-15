9245d4e453c2600f913b8468822151dc1c299c7b
Maksym Ryndia <ryndya.maxim@gmail.com>
2021-02-15 15:03:46 +0200

    Update packages dependencies + Add comments
    Update package dependencies:
    - firebase-admin: 8.13.0 -> 9.5.0
    - firebase functions: 3.9.0 -> 3.13.1
    - simple-oauth2: 3.4.0 -> 4.3.1
    Update code for `simple-oauth2` client respectively to new requirements.
    Add comments to `index.js` for better readability

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,10 @@
-const functions = require('firebase-functions')
-const express = require('express')
-const simpleOauth = require('simple-oauth2')
-const randomstring = require('randomstring')
+const functions = require('firebase-functions');
+const express = require('express');
+const simpleOauth = require('simple-oauth2');
+const randomstring = require('randomstring');
 
-const oauth = functions.config().oauth
-const oauth_provider = oauth.provider || 'github'
+const oauth = functions.config().oauth;
+const oauth_provider = oauth.provider || 'github';
 
 function getScript(mess, content) {
   return `<!doctype html><html><body><script>
@@ -21,66 +21,83 @@ function getScript(mess, content) {
     console.log("Sending message: %o", "github")
     window.opener.postMessage("authorizing:github", "*")
     })()
-  </script></body></html>`
+  </script></body></html>`;
 }
-
-const oauth2 = simpleOauth.create({
+/**
+ * Setup oauth2 configuration file
+ */
+const oauth2Config = {
   client: {
     id: oauth.client_id,
-    secret: oauth.client_secret
+    secret: oauth.client_secret,
   },
   auth: {
     tokenHost: oauth.git_hostname || 'https://github.com',
     tokenPath: oauth.token_path || '/login/oauth/access_token',
     authorizePath: oauth.authorize_path || '/login/oauth/authorize',
-  }
-})
-
-const oauthApp = express()
-
+  },
+};
+/**
+ * Initialize oauth2 client
+ */
+const oauth2 = new simpleOauth.AuthorizationCode(oauth2Config);
+/**
+ * Initialize express.js server
+ */
+const oauthApp = express();
+/**
+ * [GET] /auth
+ */
 oauthApp.get('/auth', (req, res) => {
-  const authorizationUri = oauth2.authorizationCode.authorizeURL({
+  const authorizationUri = oauth2.authorizeURL({
     redirect_uri: oauth.redirect_url,
     scope: oauth.scopes || 'repo,user',
-    state: randomstring.generate(32)
-  })
+    state: randomstring.generate(32),
+  });
 
-  res.redirect(authorizationUri)
-})
-
+  res.redirect(authorizationUri);
+});
+/**
+ * [GET] /callback
+ */
 oauthApp.get('/callback', async (req, res) => {
   var options = {
-    code: req.query.code
-  }
+    code: req.query.code,
+  };
 
   if (oauth_provider === 'gitlab') {
-    options.client_id = oauth.client_id
-    options.client_secret = oauth.client_secret
-    options.grant_type = 'authorization_code'
-    options.redirect_uri = oauth.redirect_url
+    options.client_id = oauth.client_id;
+    options.client_secret = oauth.client_secret;
+    options.grant_type = 'authorization_code';
+    options.redirect_uri = oauth.redirect_url;
   }
 
   try {
-    const result = await oauth2.authorizationCode.getToken(options)
-    const token = oauth2.accessToken.create(result)
+    const result = await oauth2.getToken(options);
+    const token = oauth2.createToken(result.token);
 
-    return res.send(getScript('success', {
-      token: token.token.access_token,
-      provider: oauth_provider
-    }))
+    return res.send(
+      getScript('success', {
+        token: token.token.access_token,
+        provider: oauth_provider,
+      })
+    );
+  } catch (error) {
+    console.error('Access Token Error', error.message);
+    res.send(getScript('error', error));
   }
-  catch (error) {
-    console.error('Access Token Error', error.message)
-    res.send(getScript('error', error))
-  }
-})
-
+});
+/**
+ * [GET] /success
+ */
 oauthApp.get('/success', (req, res) => {
-  res.send('')
-})
-
+  res.sendStatus(200);
+});
+/**
+ * [GET] /
+ */
 oauthApp.get('/', (req, res) => {
-  res.redirect(301, `/oauth/auth`)
-})
+  res.redirect(301, `/oauth/auth`);
+});
 
-exports.oauth = functions.https.onRequest(oauthApp)
+exports.oauth = functions.https.onRequest(oauthApp);
